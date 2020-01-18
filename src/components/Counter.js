d6d9b0a12a259dbe8c854e3d5c3c4df7759a2408
최철환 <hwanyhee@naver.com>
2020-01-18 17:05:47 +0900

    리팩토링

---
+++ src/components/Counter.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+export class Counter extends React.Component {
+  state = {
+    score: 30,
+    a: 1
+  }
+
+  constructor() {
+    super();
+    this.handleScore = this.handleScore.bind(this);
+  }
+
+  handleScore(delta) {
+    console.log('increment:', this);
+    // this.state.score += 1;
+    // this.setState({score: this.state.score + 1});
+    this.setState(prevState => {
+      return {score: prevState.score + delta}
+    })
+  }
+
+  render() {
+    return (
+      <div className="counter">
+        <button className="counter-action decrement" onClick={() => this.handleScore(-1)}>-</button>
+        <span className="counter-score">{this.state.score}</span>
+        <button className="counter-action increment" onClick={() => this.handleScore(1)}>+</button>
+      </div>
+    );
+  }
+}
